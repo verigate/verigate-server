c9a6d0dd58ad0956fc0ffd21968e4dff4b4ef68e
in-jun <injuninjune@gmail.com>
2025-05-13 12:28:56 +0900

    refactor: remove trailing whitespace in pkce.go
    Improve code readability by removing trailing whitespace
    
    - Improves code consistency
    - Enhances overall code style

--- internal/pkg/utils/pkce/pkce.go
+++ internal/pkg/utils/pkce/pkce.go
@@ -9,13 +9,13 @@ func VerifyCodeChallenge(codeVerifier, codeChallenge, method string) bool {
 	if method == "plain" {
 		return codeVerifier == codeChallenge
 	}
-	
+
 	if method == "S256" {
 		h := sha256.New()
 		h.Write([]byte(codeVerifier))
 		challenge := base64.RawURLEncoding.EncodeToString(h.Sum(nil))
 		return challenge == codeChallenge
 	}
-	
+
 	return false
 }
